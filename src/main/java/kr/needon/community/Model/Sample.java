94400890900c6d8553d055da4c47464b2f3dd98a
k4584587 <k4584587@naver.com>
2018-07-30 16:52:53 +0900

    예제수정

--- src/main/java/kr/needon/community/Model/Sample.java
+++ src/main/java/kr/needon/community/Model/Sample.java
@@ -1,33 +1,12 @@
 package kr.needon.community.Model;
 
+import lombok.Data;
+
+@Data
 public class Sample {
 
 	private int id;
 	private String name;
 	private String context;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getContext() {
-		return context;
-	}
-
-	public void setContext(String context) {
-		this.context = context;
-	}
 
 }
