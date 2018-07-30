aa85e33bd2c023bd03ca73ec1d3a1d900192fe7c
k4584587 <k4584587@naver.com>
2018-07-30 17:11:03 +0900

    시큐리티 적

---
+++ src/main/java/kr/needon/community/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package kr.needon.community.Config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//=====================================
+// 	클래스 설명 : 시큐리티 설정 클래스
+//	작성자 : 김현우
+//=====================================
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
