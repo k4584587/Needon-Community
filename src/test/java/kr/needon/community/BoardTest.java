e8cdf30dd529333669f0304b638a0285bd087c11
k4584587 <k4584587@naver.com>
2018-08-02 11:33:11 +0900

    junit 에 필요한 라이브러리 추가

--- src/test/java/kr/needon/community/BoardTest.java
+++ src/test/java/kr/needon/community/BoardTest.java
@@ -1,26 +1,37 @@
 package kr.needon.community;
 
-import java.util.List;
-
+import kr.needon.community.Model.Board;
+import kr.needon.community.Module.Board.BoardDAOImpl;
+import kr.needon.community.Module.Board.BoardServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import kr.needon.community.Model.Board;
-import kr.needon.community.Module.Board.BoardDAOImpl;
-import kr.needon.community.Module.Board.BoardServiceImpl;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
 public class BoardTest {
+
+	@Mock
+	HttpServletRequest request;
 
 	@Autowired
 	private BoardServiceImpl service;
 	
 	@Autowired
 	private BoardDAOImpl dao;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
 	public void List() throws Exception {
@@ -37,7 +48,8 @@ public class BoardTest {
 		board.setIp("192.168.30.8");
 		board.setWr_no(1);
 
-		Boolean result = service.insert(board);
+
+		Boolean result = service.insert(request,board);
 		System.out.println("결과 ==> " + result);
 	}
 	
