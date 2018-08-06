954f6a87a0c317d5a26cc6edc9a0fc8098d2e7f7
leon9329 <msclown2@gmail.com>
2018-08-06 16:33:00 +0900

    test

--- src/main/java/kr/needon/community/Controller/UserController.java
+++ src/main/java/kr/needon/community/Controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
     
     @RequestMapping(value="/userJoinForm")
     public String userJoinForm() {
-    	return "user/userJoinForm"; 
+    	return "user/userJoinForm";  
     }
 
 }
