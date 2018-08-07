3919c303109d63b1d853920cf58be659ff641c71
k4584587 <k4584587@naver.com>
2018-08-07 09:11:42 +0900

    .

--- src/main/java/kr/needon/community/Controller/AdminController.java
+++ src/main/java/kr/needon/community/Controller/AdminController.java
@@ -1,5 +1,7 @@
 package kr.needon.community.Controller;
 
+import kr.needon.community.Model.Member;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ public class AdminController {
 
     @GetMapping("/")
     public String AdminMain() {
+
+        Member member = (Member) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("로그인한 사용자 ==> " + member.toString());
 
         return "admin/admin";
     }
